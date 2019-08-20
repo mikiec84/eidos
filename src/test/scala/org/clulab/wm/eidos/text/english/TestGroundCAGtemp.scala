372dbfbf1721b4972685af9d7405e4bf09eb71b9
zupon <zupon@email.arizona.edu>
2019-08-19 18:15:22 -0700

    worked on grounding CAG links; now need to plug into the right place (apps?)

--- src/test/scala/org/clulab/wm/eidos/text/english/TestGroundCAGtemp.scala
+++ src/test/scala/org/clulab/wm/eidos/text/english/TestGroundCAGtemp.scala
@@ -1,5 +1,7 @@
 package org.clulab.wm.eidos.text.english
 
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+
 import ai.lum.common.ConfigUtils._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.clulab.processors.Processor
@@ -40,9 +42,11 @@ class TestGroundCAGtemp extends EnglishTest{
   val lines: Array[String] = fileString.split("\n")
 //  file.close()
 
+  var newFileArray = Array(lines.head)
+
   for (line <- lines.tail) {
     var index = lines.indexOf(line).toString()
-    println("\n\nINDEX:\t" + index)
+//    println("\n\nINDEX:\t" + index)
 
     var splitLine = line.split("\t")
 
@@ -52,8 +56,8 @@ class TestGroundCAGtemp extends EnglishTest{
 
 
     // get cause/effect nodes for grounding
-    val sourceNode = splitLine(5)
-    val targetNode = splitLine(8)
+    var sourceNode = splitLine(5)
+    var targetNode = splitLine(8)
 
     // decide how many groundings to take
     val topk = 5
@@ -66,18 +70,47 @@ class TestGroundCAGtemp extends EnglishTest{
     val topTargetGroundings = targetNodeGrounder.take(topk).toArray
 
 
-    println("\nSOURCE TEXT:\t" + sourceText)
-    println("SOURCE GROUNDINGS:")
+//    println("\nSOURCE TEXT:\t" + sourceText)
+//    println("SOURCE GROUNDINGS:")
+    var justSourceGroundings = Array[String]()
     for (grounding <- topSourceGroundings) {
-      println(grounding._1) // only prints grounding, not score
+      justSourceGroundings = justSourceGroundings :+ grounding._1.toString
+//      println(grounding._1) // only prints grounding, not score
     }
 
-    println("\nTARGET TEXT:\t" + targetText)
-    println("TARGET GROUNDINGS:")
+//    println("\nTARGET TEXT:\t" + targetText)
+//    println("TARGET GROUNDINGS:")
+    var justTargetGroundings = Array[String]()
     for (grounding <- topTargetGroundings) {
-      println(grounding._1) // only prints grounding, not score
+      justTargetGroundings = justTargetGroundings :+ grounding._1.toString
+//      println(grounding._1) // only prints grounding, not score
     }
+
+    sourceNode = justSourceGroundings.mkString(" ")
+    targetNode = justTargetGroundings.mkString(" ")
+
+    val firstPart = splitLine.slice(0,6)
+    val middlePart = splitLine.slice(6,9)
+
+//    val lastElement = splitLine.last//.indexOf()
+//    println("LAST ELEMENT:\t" + lastElement)
+    val lastPart = splitLine.slice(9,splitLine.length)
+
+
+    val newLine = firstPart ++ Array(sourceNode) ++ middlePart ++ Array(targetNode) ++ lastPart
+//    val newLine2 = newLine ++ middlePart
+//    val newLine3 = newLine2 ++ Array(targetNode)
+//    val newLine4 = newLine3 ++ lastPart
+
+//    println("NEW LINE:\n" + newLine.mkString("\t"))
+    newFileArray = newFileArray :+ newLine.mkString("\t")
   }
 
+  println("NEW FILE ARRAY:\n" + newFileArray.mkString("\n"))
+
+  // makes new file with columns filled by source/target nodes; should be same file?
+  val writer = new PrintWriter(new File("groundedCAGlinks.tsv"))
+  writer.write(newFileArray.mkString("\n"))
+  writer.close()
 
 }
